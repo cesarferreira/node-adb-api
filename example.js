7ba12a9baf2fdc7127e1d98a24d357cfe1a45a4f
Cesar Ferreira <cesar.ferreira@wayve.ai>
2024-06-20 15:10:28 +0100

    bumped shelljs

--- example.js
+++ example.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
 const log = console.log;
-const adb = require('./api.js');
+const adb = require("./api.js");
 
 // Use the `adb` as you please
-log(adb.getListOfDevices())
-adb.downloadAPK('ab993982', 'com.amazon.mShop.android.shopping')
+const list = adb.getListOfDevices();
+
+log(list);
+log(list[0]);
+// adb.downloadAPK("ab993982", "com.amazon.mShop.android.shopping");
